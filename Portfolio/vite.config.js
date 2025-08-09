bae5e016a85e6495143349ba32333e5100ba24bf
ChuChu145 <bladger1@gmail.com>
2025-08-09 16:14:56 +0100

    Update vite.config.js

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -1,7 +1,18 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  base: './', 
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync('*.html').map(file => [
+          file.replace(/\.html$/, ''),
+          resolve(__dirname, file)
+        ])
+      )
+    }	
+  }
+});
+
